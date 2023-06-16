900a8bc51b89a9301aa6fdfdfb8c75ee8433f0ea
staceytay <staceytay@users.noreply.github.com>
2023-06-16 10:55:06 -0400

    Minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -107,13 +107,6 @@ fn build_query(domain_name: String, record_type: u16) -> Vec<u8> {
     query
 }
 
-// From https://doc.rust-lang.org/std/primitive.u16.html#method.from_be_bytes
-fn read_be_u16(input: &mut &[u8]) -> u16 {
-    let (int_bytes, rest) = input.split_at(std::mem::size_of::<u16>());
-    *input = rest;
-    u16::from_be_bytes(int_bytes.try_into().unwrap())
-}
-
 #[derive(Debug)]
 struct DNSRecord {
     name: Vec<u8>,
@@ -127,46 +120,38 @@ struct DNSPacket {}
 
 impl DNSRecord {
     fn parse_record(buf: &[u8], cursor_start: usize) -> (usize, DNSRecord) {
-        println!("parse_record: cursor_start = {}", cursor_start);
-        let mut cursor: usize = cursor_start;
+        let mut cursor = cursor_start;
+
         let (length, name) = decode_name(buf, cursor);
         cursor += length;
 
-        println!("parse_record: cursor = {}", cursor);
-        println!("parse_record: length = {}, name = {}", length, name.clone());
-        println!("{}", show(&buf[cursor..]));
+        let (type_, class, ttl, data_length) = (
+            u16::from_be_bytes(buf[cursor..cursor + 2].try_into().unwrap()),
+            u16::from_be_bytes(buf[cursor + 2..cursor + 4].try_into().unwrap()),
+            u32::from_be_bytes(buf[cursor + 4..cursor + 8].try_into().unwrap()),
+            (u16::from_be_bytes(buf[cursor + 8..cursor + 10].try_into().unwrap()) as usize),
+        );
+        cursor += 10;
 
-        let data_length: usize =
-            u16::from_be_bytes(buf[cursor + 8..cursor + 10].try_into().unwrap()).into();
-        let dns_record = DNSRecord {
-            name: name.into(),
-            type_: u16::from_be_bytes(buf[cursor..cursor + 2].try_into().unwrap()),
-            class: u16::from_be_bytes(buf[cursor + 2..cursor + 4].try_into().unwrap()),
-            ttl: u32::from_be_bytes(buf[cursor + 4..cursor + 8].try_into().unwrap()),
-            data: (&buf[cursor + 10..cursor + 10 + data_length]).to_vec(),
-        };
-        cursor += (10 + data_length);
+        let data = (&buf[cursor..cursor + data_length]).to_vec();
+        cursor += data_length;
 
-        (cursor - cursor_start, dns_record)
-
-        //         name = decode_name(reader)
-        // data = reader.read(10)
-        // type_, class_, ttl, data_len = struct.unpack("!HHIH", data)
-        // data = reader.read(data_len)
-        // return DNSRecord(name, type_, class_, ttl, data)
+        (
+            cursor - cursor_start,
+            DNSRecord {
+                name: name.into(),
+                type_,
+                class,
+                ttl,
+                data,
+            },
+        )
     }
 }
 
 impl DNSPacket {
     // fn parse(&self, buf: &[u8]) -> DNSPacket {}
 }
-
-// TODO: Right now it's a big mess where cursor is treated as length and vice
-// versa, need to standardise what to do so that I can read the buffer from
-// caller and handle the indexing correctly
-// One approach is to pass in a cursor but just return the length read? And use
-// this to avoid the hardcording within function
-//
 
 // Returns length of bytes read from buf and decoded name.
 fn decode_name(buf: &[u8], cursor_start: usize) -> (usize, String) {
@@ -196,26 +181,9 @@ fn decode_name(buf: &[u8], cursor_start: usize) -> (usize, String) {
 }
 
 fn decode_compressed_name(buf: &[u8], cursor_start: usize) -> String {
-    println!(
-        "decode_compressed_name: buf[cursor_start] = {},  buf[cursor_start] & 0b00111111 = {}, buf[cursor_start + 1] = {}",
-        buf[cursor_start],
-        buf[cursor_start] & 0b00111111,
-        buf[cursor_start + 1],
-    );
-    let pointer = u16::from_be_bytes([(buf[cursor_start] & 0b00111111), buf[cursor_start + 1]]);
-    println!("decode_compressed_name: pointer = {}", pointer);
-    decode_name(buf, pointer as usize).1
-
-    // println!("decode_compressed_name: {}: {}", name_length, name.clone());
-    // String::from("TODO")
-
-    // //    pointer_bytes = bytes([]) + reader.read(1)
-    // pointer = struct.unpack("!H", pointer_bytes)[0]
-    // current_pos = reader.tell()
-    // reader.seek(pointer)
-    // result = decode_name(reader)
-    // reader.seek(current_pos)
-    // return result
+    let pointer =
+        u16::from_be_bytes([(buf[cursor_start] & 0b00111111), buf[cursor_start + 1]]) as usize;
+    decode_name(buf, pointer).1
 }
 
 fn main() -> std::io::Result<()> {
@@ -230,7 +198,7 @@ fn main() -> std::io::Result<()> {
         let mut buf = [0; 1024];
         let (amt, src) = socket.recv_from(&mut buf)?;
 
-        println!("{}", show(&buf[..]));
+        // println!("{}", show(&buf[..]));
 
         let h = DNSHeader::parse_header(&buf[..]);
         println!("h: {:?}", h);
