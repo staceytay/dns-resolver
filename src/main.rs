c3511386025384b187ae476df09995b105e3e973
staceytay <staceytay@users.noreply.github.com>
2023-06-16 10:14:56 -0400

    Working parse_record with DNS compression

--- src/main.rs
+++ src/main.rs
@@ -52,8 +52,8 @@ impl DNSHeader {
 
 impl DNSQuestion {
     // TODO: Return as Result.
-    fn parse_question(buf: &[u8]) -> (usize, DNSQuestion) {
-        let (length, name) = decode_name(buf);
+    fn parse_question(buf: &[u8], cursor: usize) -> (usize, DNSQuestion) {
+        let (length, name) = decode_name(buf, cursor);
         let cursor = length + 1;
         let type_ = u16::from_be_bytes(buf[cursor..cursor + 2].try_into().unwrap());
         let class = u16::from_be_bytes(buf[cursor + 2..cursor + 4].try_into().unwrap());
@@ -114,38 +114,108 @@ fn read_be_u16(input: &mut &[u8]) -> u16 {
     u16::from_be_bytes(int_bytes.try_into().unwrap())
 }
 
+#[derive(Debug)]
 struct DNSRecord {
     name: Vec<u8>,
     type_: u16,
     class: u16,
-    ttl: u16,
+    ttl: u32,
     data: Vec<u8>,
 }
 
 struct DNSPacket {}
 
 impl DNSRecord {
-    fn parse_record() {}
+    fn parse_record(buf: &[u8], cursor_start: usize) -> (usize, DNSRecord) {
+        println!("parse_record: cursor_start = {}", cursor_start);
+        let mut cursor: usize = cursor_start;
+        let (length, name) = decode_name(buf, cursor);
+        cursor += length;
+
+        println!("parse_record: cursor = {}", cursor);
+        println!("parse_record: length = {}, name = {}", length, name.clone());
+        println!("{}", show(&buf[cursor..]));
+
+        let data_length: usize =
+            u16::from_be_bytes(buf[cursor + 8..cursor + 10].try_into().unwrap()).into();
+        let dns_record = DNSRecord {
+            name: name.into(),
+            type_: u16::from_be_bytes(buf[cursor..cursor + 2].try_into().unwrap()),
+            class: u16::from_be_bytes(buf[cursor + 2..cursor + 4].try_into().unwrap()),
+            ttl: u32::from_be_bytes(buf[cursor + 4..cursor + 8].try_into().unwrap()),
+            data: (&buf[cursor + 10..cursor + 10 + data_length]).to_vec(),
+        };
+        cursor += (10 + data_length);
+
+        (cursor - cursor_start, dns_record)
+
+        //         name = decode_name(reader)
+        // data = reader.read(10)
+        // type_, class_, ttl, data_len = struct.unpack("!HHIH", data)
+        // data = reader.read(data_len)
+        // return DNSRecord(name, type_, class_, ttl, data)
+    }
 }
 
 impl DNSPacket {
     // fn parse(&self, buf: &[u8]) -> DNSPacket {}
 }
 
-fn decode_name(buf: &[u8]) -> (usize, String) {
+// TODO: Right now it's a big mess where cursor is treated as length and vice
+// versa, need to standardise what to do so that I can read the buffer from
+// caller and handle the indexing correctly
+// One approach is to pass in a cursor but just return the length read? And use
+// this to avoid the hardcording within function
+//
+
+// Returns length of bytes read from buf and decoded name.
+fn decode_name(buf: &[u8], cursor_start: usize) -> (usize, String) {
+    println!("decode_name: cursor_start = {}", cursor_start);
+    let mut cursor: usize = cursor_start;
     let mut labels: Vec<String> = Vec::new();
-    let mut cursor: usize = 0;
-    let mut length: usize = buf[0].into();
+    let mut length: usize = buf[cursor].into();
 
     while length != 0 {
-        // Ignore length value in `start`.
-        let (start, end) = (cursor + 1, cursor + length + 1);
-        labels.push(String::from_utf8((&buf[start..end]).to_vec()).unwrap());
-        cursor += length + 1;
-        length = buf[cursor].into();
+        if ((length as u8) & 0b11000000) != 0 {
+            println!("decode_name: bitwise triggered at {}", cursor);
+            // Assumption: if the name is compressed, we'll get the entire domain name here.
+            labels.push(decode_compressed_name(buf, cursor));
+            cursor += 2;
+            break;
+            // return (2, labels.join("."));
+        } else {
+            // Ignore length value in `start`.
+            let (start, end) = (cursor + 1, cursor + length + 1);
+            labels.push(String::from_utf8((&buf[start..end]).to_vec()).unwrap());
+            cursor += length + 1;
+            length = buf[cursor].into();
+        }
     }
 
-    (cursor, labels.join("."))
+    (cursor - cursor_start, labels.join("."))
+}
+
+fn decode_compressed_name(buf: &[u8], cursor_start: usize) -> String {
+    println!(
+        "decode_compressed_name: buf[cursor_start] = {},  buf[cursor_start] & 0b00111111 = {}, buf[cursor_start + 1] = {}",
+        buf[cursor_start],
+        buf[cursor_start] & 0b00111111,
+        buf[cursor_start + 1],
+    );
+    let pointer = u16::from_be_bytes([(buf[cursor_start] & 0b00111111), buf[cursor_start + 1]]);
+    println!("decode_compressed_name: pointer = {}", pointer);
+    decode_name(buf, pointer as usize).1
+
+    // println!("decode_compressed_name: {}: {}", name_length, name.clone());
+    // String::from("TODO")
+
+    // //    pointer_bytes = bytes([]) + reader.read(1)
+    // pointer = struct.unpack("!H", pointer_bytes)[0]
+    // current_pos = reader.tell()
+    // reader.seek(pointer)
+    // result = decode_name(reader)
+    // reader.seek(current_pos)
+    // return result
 }
 
 fn main() -> std::io::Result<()> {
@@ -160,15 +230,17 @@ fn main() -> std::io::Result<()> {
         let mut buf = [0; 1024];
         let (amt, src) = socket.recv_from(&mut buf)?;
 
+        println!("{}", show(&buf[..]));
+
         let h = DNSHeader::parse_header(&buf[..]);
-        println!("{:?}", h);
+        println!("h: {:?}", h);
 
-        let (len, q) = DNSQuestion::parse_question(&buf[12..]);
-        println!("{:?}", q);
-        println!("{:?}", q.name);
-        println!("{:?}", String::from("www.example.com"));
+        const HEADER_LENGTH: usize = 12;
+        let (q_len, q) = DNSQuestion::parse_question(&buf[..], HEADER_LENGTH);
+        println!("q: {}: {:?}", q_len, q);
 
-        println!("{}", show(&buf[12 + len..]));
+        let (r_len, r) = DNSRecord::parse_record(&buf[..], HEADER_LENGTH + q_len);
+        println!("r: {}: {:?}", r_len, r);
     } // the socket is closed here
     Ok(())
 }
